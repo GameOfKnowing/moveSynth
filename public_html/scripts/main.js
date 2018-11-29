4e063c4fb9cafdf7c4d913419ad7e7eeb8db13d9
Laura Lytle <laura_lytle@aol.com>
2018-11-29 18:14:41 +0000

    Adds basic location-based note player

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -1,16 +1,19 @@
-var pgOut, trackId, isTrack, position;
+var Tone = require('tone');
+var synth, sLat, sLon, sAlt, pgOut, trackId, isTrack, position, freq, nLen, fMult;
+fMult = 1;
+synth = new Tone.Synth().toMaster();	
 
 window.onload = function buffer(){
 	pgOut = document.getElementById("myLocation");
 }
 
-function startTrack() {
-    isTrack = true;
-    if (navigator.geolocation) {
-	trackId = navigator.geolocation.watchPosition(showPosition);
-    } else { 
-        pgOut.innerHTML = "Geolocation is not supported by this browser.";}
-    }
+window.startTrack = function(){ //starts GPS tracking
+	isTrack = true;
+	if (navigator.geolocation) {
+		trackId = navigator.geolocation.watchPosition(showPosition);
+	} else { 
+		pgOut.innerHTML = "Geolocation is not supported by this browser.";}
+}
     
 function showPosition(position) {
     	pgOut.innerHTML= "TRACKING<br>" +
@@ -19,12 +22,34 @@ function showPosition(position) {
 	if (position.coords.altitude) {
 		pgOut.innerHTML = pgOut.innerHTML + "<br>Altitude: " + position.coords.altitude;
 	}
+	initSynth(position)
 }
 
-function stopTrack() {
+function initSynth(position){  //Sanitizes position data for synth inpu and initializes synth
+	sLat = Math.trunc((position.coords.latitude * 1000)%1000);
+	sLon = Math.trunc((position.coords.longitude * 1000)%1000);
+	var lenPow = 1 + (Math.abs(position.coords.latitude) / 22.5);
+	lenPow = Math.trunc(lenPow);
+	nLen = Math.pow(2, lenPow) + "n";
+	//console.log(nLen);
+	if (position.coords.altitude) {
+		sAlt = Math.trunc(position.coords.altitude * 10);
+		freq = Math.abs((sAlt+ Math.abs(sLon) + Math.abs(sLat))  * fMult);
+		pgOut.innerHTML = pgOut.innerHTML + "<br>" + freq;
+	} else {
+		freq = (Math.abs(sLat) + Math.abs(sLon)) * fMult;
+	}
+}
+
+window.stopTrack = function() {  //Stops GPS tracking
 	if (isTrack == true) {
 		isTrack = false;
 		pgOut.innerHTML="NOT " + pgOut.innerHTML;
 		navigator.geolocation.clearWatch(trackId);
 	}
 }
+
+window.playNote = function(){
+	synth.triggerAttackRelease(freq, nLen);
+}
+
